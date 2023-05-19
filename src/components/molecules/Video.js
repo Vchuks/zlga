444e48caf4ae4cba540e409d36272fda3971104d
Vchuks <64593598+Vchuks@users.noreply.github.com>
2023-05-19 17:02:42 +0100

    video

--- src/components/molecules/Video.js
+++ src/components/molecules/Video.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactPlayer from "react-player";
+import vid from '../../images/vid.mp4'
 
 const Video = () => {
   return (
     <div className="w-100 d-flex justify-content-center p-3 py-lg-5">
       <ReactPlayer
-        url="https://zlglobalalliance.com.ng/zlgavideo/zlg.mp4"
+        url={vid}
         width="90%"
         height="100%"
         controls
