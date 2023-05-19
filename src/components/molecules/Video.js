53436878ac0569a60026309fe48e9f298095642d
Vchuks <64593598+Vchuks@users.noreply.github.com>
2023-05-19 15:15:28 +0100

    update

--- src/components/molecules/Video.js
+++ src/components/molecules/Video.js
@@ -1,46 +1,15 @@
 import React from "react";
-// import vid from "../../images/vid.mp4"
-
-import ReactPlayer from 'react-player';
-
+import ReactPlayer from "react-player";
 
 const Video = () => {
-  // const src = ""
-  
-  // const handlePlay = () => {
-  //   const videocard = document.querySelector("#video1");
-  //   const videoWrapper = document.querySelector(".video");
-  //   const wrapper = document.querySelector(".wrapper");
-  //   const close =document.querySelector('.ex');
-  //   const playimg =document.querySelector('.playimg');
-  //   playimg.style.display="none"
-  //   videocard.play();
-  //   videoWrapper.style.display = "block";
-  //   close.style.display="block";
-  //   wrapper.classList.add('wrapper2') 
-  //   wrapper.classList.remove('wrapper') 
-  // };
-  
-
-  
-  // const handleClose = () => {
-  //   const videoWrapper = document.querySelector(".video");
-  //   const wrapper = document.querySelector(".wrapper2");
-  //   const videocard = document.querySelector("#video1");
-  //   const playimg =document.querySelector('.playimg');
-  //   const close =document.querySelector('.ex');
-  //   playimg.style.display="block"
-  //   close.style.display="none"
-  //   videocard.pause();
-  //   videocard.currentTime = 0;
-  //   videoWrapper.style.display = "none";
-  //   wrapper.classList.add('wrapper')
-  //   wrapper.classList.remove('wrapper2')
-  // };
-
   return (
-    <div className="w-100 d-flex justify-content-center">
-<ReactPlayer url="../../images/vid.mp4" width="100%" height="100%" controls />
+    <div className="w-100 d-flex justify-content-center p-3 py-lg-5">
+      <ReactPlayer
+        url="https://zlglobalalliance.com.ng/zlgavideo/zlg.mp4"
+        width="90%"
+        height="100%"
+        controls
+      />
     </div>
   );
 };
