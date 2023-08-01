c1d52ccfc3fcec1519f2928543588c528f089516
Vchuks <64593598+Vchuks@users.noreply.github.com>
2023-08-01 14:31:08 +0100

    change in nav

--- src/components/molecules/Navbar.js
+++ src/components/molecules/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Image from "../atom/Image";
 import logo from "../../images/ZLGA LOGO 1.png";
 import closeicon from "../../images/iconoir_cancel.png";
@@ -6,9 +6,24 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const [open, setOpen] = useState(true)
+  const navref = useRef()
+
+  const handleOptions = ()=>{
+   
+      if(navref.current.className === "navw navbar-collapse justify-content-lg-center collapse show" ){
+        navref.current.className = "navw navbar-collapse justify-content-lg-center collapse"
+      setOpen(!open)
+    }else {
+      setOpen(!open)
+      navref.current.className = "navw navbar-collapse justify-content-lg-center collapse show"
+
+      }
+      
+  }
+
   return (
     <>
-      <nav className={`navbar ${open ? "nnav" : "nnav2"} sticky-top navbar-expand-lg py-2 px-lg-5`}>
+      <nav  className={`navbar ${open ? "nnav" : "nnav2"} sticky-top navbar-expand-lg py-2 px-lg-5`}>
         <div className="w-100  container-fluid d-flex justify-content-between p-0">
           <NavLink to="/" className={`ps-3 ps-lg-0 navbar-brand ${open ? "navtext" : "navtext2"} `}>
             <Image
@@ -31,7 +46,7 @@ const Navbar = () => {
             {open ?<span className="text-dark navbar-toggler-icon"></span>:
             <span className=""><Image src={closeicon} className="closs"/></span>}
           </button>
-          <div className=" navw collapse navbar-collapse justify-content-lg-center" id="navbarText">
+          <div className=" navw collapse navbar-collapse justify-content-lg-center" ref={navref} id="navbarText">
             <ul className=" navbar-nav mb-2 mb-lg-0">
               <li className="nav-item dropdown ">
                 <NavLink
@@ -45,34 +60,34 @@ const Navbar = () => {
                 </NavLink>
                 <ul className="dropdown-menu">
                   <li>
-                    <NavLink className="dropdown-item navv" to="/school">
+                    <NavLink onClick={handleOptions} className="dropdown-item navv" to="/school">
                       School
                     </NavLink>
                   </li>
                   <li>
-                    <NavLink className="dropdown-item navv" to="/medical">
+                    <NavLink className="dropdown-item navv" onClick={handleOptions} to="/medical">
                       Medical Services
                     </NavLink>
                   </li>
                   <li>
-                    <NavLink className="dropdown-item navv" to="/environment">
+                    <NavLink className="dropdown-item navv" onClick={handleOptions} to="/environment">
                       Environmental Management
                     </NavLink>
                   </li>
                 </ul>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link navv" to="/about">
+                <NavLink className="nav-link navv" onClick={handleOptions} to="/about">
                   About Us
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link navv" to="/store">
+                <NavLink className="nav-link navv" onClick={handleOptions} to="/store">
                   ZLGA Store
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link navv" to="/contact">
+                <NavLink className="nav-link navv" onClick={handleOptions} to="/contact">
                   Contact Us
                 </NavLink>
               </li>
@@ -88,17 +103,17 @@ const Navbar = () => {
                 </NavLink>
                 <ul className="dropdown-menu">
                   <li>
-                    <NavLink className="dropdown-item navv" to="/career">
+                    <NavLink className="dropdown-item navv" onClick={handleOptions} to="/career">
                       Career
                     </NavLink>
                   </li>
                   <li>
-                    <NavLink className="dropdown-item navv" to="/faq">
+                    <NavLink className="dropdown-item navv" onClick={handleOptions} to="/faq">
                       FAQs
                     </NavLink>
                   </li>
                   <li>
-                    <NavLink className="dropdown-item navv" to="/gallery">
+                    <NavLink className="dropdown-item navv" onClick={handleOptions} to="/gallery">
                       Gallery
                     </NavLink>
                   </li>
